fbe769682e00205422cab3ba547df1d94084867f
dadvarzanamin <info@dadvarzanamin.ir>
2025-12-26 20:51:25 +0330

    v5.0.98

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -23,32 +23,32 @@ class InvoiceController extends Controller
         $invoice->save();
 
         $invoices = DB::table('invoices')
-        ->leftJoin('workshops', function ($join) {
-            $join->on('invoices.product_id', '=', 'workshops.id')
-                ->where('invoices.product_type', '=', 'workshop');
-        })
-        ->leftJoin('contracts', function ($join) {
-            $join->on('invoices.product_id', '=', 'contracts.id')
-                ->where('invoices.product_type', '=', 'contracts');
-        })
-        ->leftJoin('estelams', function ($join) {
-            $join->on('invoices.product_id', '=', 'estelams.id')
-                ->where('invoices.product_type', '=', 'estelam');
-        })
-        ->where('invoices.user_id', Auth::id())
-        ->where('invoices.price_status', 4)
-        ->select(
-            'invoices.*',
-            DB::raw("CASE
+            ->leftJoin('workshops', function ($join) {
+                $join->on('invoices.product_id', '=', 'workshops.id')
+                    ->where('invoices.product_type', '=', 'workshop');
+            })
+            ->leftJoin('contracts', function ($join) {
+                $join->on('invoices.product_id', '=', 'contracts.id')
+                    ->where('invoices.product_type', '=', 'contracts');
+            })
+            ->leftJoin('estelams', function ($join) {
+                $join->on('invoices.product_id', '=', 'estelams.id')
+                    ->where('invoices.product_type', '=', 'estelam');
+            })
+            ->where('invoices.user_id', Auth::id())
+            ->where('invoices.price_status', 4)
+            ->select(
+                'invoices.*',
+                DB::raw("CASE
             WHEN invoices.product_type = 'workshop' THEN workshops.title
             WHEN invoices.product_type = 'contracts' THEN contracts.title
             WHEN invoices.product_type = 'estelam' THEN estelams.title_fa
             ELSE NULL END AS product_name"),
-            DB::raw("CASE
+                DB::raw("CASE
             WHEN invoices.product_type = 'contracts' THEN contracts.file_path
             ELSE NULL END AS file_path")
-        )
-        ->get();
+            )
+            ->get();
 
         return response()->json(
             ['isSuccess' => true,
@@ -56,6 +56,24 @@ class InvoiceController extends Controller
                 'errors' => null,
                 'status_code' => 200,
                 'result' => $invoices
+            ], 200);
+    }
+
+    public function setinvoice(Request $request)
+    {
+        $invoice = new Invoice();
+        $invoice->user_id           = Auth::user()->id;
+        $invoice->product_id        = $request->input('id');
+        $invoice->product_type      = $request->input('product_type');
+        $invoice->product_price     = $request->input('product_price');
+        $invoice->save();
+
+        return response()->json(
+            ['isSuccess' => true,
+                'message' => 'مقادیر رکورد دریافت شد',
+                'errors' => null,
+                'status_code' => 200,
+                'result' => $invoice
             ], 200);
     }
 
